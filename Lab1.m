39b4346682e7cf471ad4d37ec3bdd82aa669a22c
oscbck-5 <45263726+oscbck-5@users.noreply.github.com>
2019-02-15 17:42:51 +0100

    Add files via upload

--- Lab1.m
+++ Lab1.m
@@ -1,6 +1,5 @@
 %% System Param
 %h vs height Voltage in cm
-clc
 h = 5:1:15;
 hV = [1.826, 2.539, 3.452, 4.18, 4.99, 5.762, 6.665, 7.36, 8.193, 8.93, 9.66];
 
@@ -15,8 +14,8 @@ vt = [21.18, 26.66 30.97, 34.28, 37.92, 39.73, 42.53, 45.45, 46.49, 47.67];
 C1=[1 0];
 C2=[0 1];
 C=[C1;C2];
-R= [0.1 0
-   0 0.1];
+R= [0.05 0
+   0 0.05];
 
 g = 981;
 %A = 3.5e-3;
@@ -25,20 +24,19 @@ A=33;
 a=0.17;
 h10=7;
 h20=h10;
-h0=0;
+h0=3.2;
 Ac=[-(a*g*sqrt(2))/(2*sqrt((h10+h0)*g)*A) 0
     (a*g*sqrt(2))/(2*sqrt((h10+h0)*g)*A) -(a*g*sqrt(2))/(2*sqrt((h20+h0)*g)*A)];
 
 Bc=[1/A
     0];
 N=[1
-   0];
+   1];
 
-[P L K]=care(Ac',C2',eye(2)*R(1,1),R(2,2))
-K = K';
-%K=(P*C2'+R(1,2))*inv(R(2,2))    
+P=care(Ac',C2',eye(2)*R(1,1),R(2,2));
+
+K=(P*C2'+R(1,2))*inv(R(2,2))
 %% Discrete
-clc
 Plant=ss(Ac,Bc,C2,0);
 %[kalmf,K,P,M] = kalman(Plant,1,1);
 
@@ -46,9 +44,8 @@ Plant=ss(Ac,Bc,C2,0);
 PlantD=c2d(Plant,0.005);
 [Ak,Bk,Ck,Dk]=ssdata(PlantD);
 
-[P L K]=dare(Ak',Ck',eye(2)*R(1,1),R(2,2))
+[P L K]=dare(Ak',Ck',eye(2)*R(1,1),R(2,2),N*R(1,2),eye(2));
 K = K'
-%K = K'
-%K=(P*Ck'+R(1,2))*inv(R(2,2))
+%K=(P*Ck'+R(1,2))*inv(R(2,2));
 
-
+%Kt=P*Ck'*(Ck*P*Ck'+R(2,2))^(-1);
